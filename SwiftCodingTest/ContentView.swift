a5e574a7faaa0302cad4d692b0191c8f81764383
DanielAlert <alertwisdomsolutions@gmail.com>
2019-10-01 13:45:48 -0400

    Initial Commit

---
+++ SwiftCodingTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftCodingTest
+//
+//  Created by DANIEL SPADY on 10/1/19.
+//  Copyright © 2019 DANIEL SPADY. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
